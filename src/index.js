33bfbf060b4fe4a5d44599aef0ef19893e2d9e11
NA-Siddiky <siddiky.academic@gmail.com>
2024-03-31 13:26:13 +0600

    allow cors origin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 require("./db/mongoose");
+const cors = require("cors");
 
 const taskRouter = require("./routes/task");
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 app.use(taskRouter);
 
